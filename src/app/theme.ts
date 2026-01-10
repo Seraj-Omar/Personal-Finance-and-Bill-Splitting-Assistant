a3aa38d80fa71adfb75c2002ed80a8faafc5e62e
noorafifi889 <noorelafifi889@gmail.com>
2026-01-10 19:43:45 +0200

    add a main gradient color to global css

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,11 +1,14 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles"
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '',
+      main: "rgba(52, 71, 170, 0.87)",
+    },
+    secondary: {
+      main: "rgba(246, 227, 231, 0.84)",
     },
   },
-});
+})
 
-export default theme;
+export default theme
