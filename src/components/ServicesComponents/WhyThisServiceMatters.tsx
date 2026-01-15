a806e74134c93dcc4599daa3375e397b1921cc8c
Seraj-Omar <srag20062017@gmail.com>
2026-01-15 18:38:53 +0200

    Services Page Ready except for the gradient of the WhyMatters component background gradient

--- src/components/ServicesComponents/WhyThisServiceMatters.tsx
+++ src/components/ServicesComponents/WhyThisServiceMatters.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Box, Typography } from "@mui/material";
 import { Check } from "lucide-react";
-
+import ContactUsForm from "./ContactUsForm";
 const featuresList = [
     "Secure wallet storage",
     "Real-time portfolio tracking",
@@ -16,8 +16,12 @@ const progressList = [
 
 export default function WhyChooseUs() {
     return (
-        <Box className="flex flex-col p-[24px] bg-[#FFF] rounded-xl ">
-            <Box className="flex flex-row gap-[24px]">
+        <Box className="flex flex-col p-[24px] bg-[#FFF] rounded-xl flex-wrap gap-[48px]">
+
+            {/* top part */}
+            <Box className="flex flex-row gap-[24px] flex-wrap">
+
+                {/*left side*/}
                 <Box className="w-[483px] h-[272px] overflow-hidden rounded-xl">
                     <Box component="img" src="/whyThisServiceMatters.jpg" alt="Employee showing financial graph"
                         sx={{
@@ -27,6 +31,8 @@ export default function WhyChooseUs() {
                         }}
                     />
                 </Box>
+
+                {/*right side*/}
                 <Box className="flex flex-col gap-[16px] justify-center items-start">
                     <Typography className="w-fit text-[#1C1A1A]">
                         Why This Service Matters
@@ -39,6 +45,75 @@ export default function WhyChooseUs() {
                         Clear financial tracking helps you understand your money better and take control with confidence.
                     </Typography>
                 </Box>
+            </Box>
+
+
+            {/* bottom part */}
+            <Box className="flex flex-row justify-between gap-[24px] flex-wrap">
+                
+                {/*left side*/}
+                <Box className="flex flex-col gap-[16px] justify-center items-start">
+                    <Typography className="w-fit text-[#1C1A1A] !text-[24px] !font-[500] max-w-[310px] !leading-[25px]">
+                        We help you understand where your money goes.
+                    </Typography>
+                    <Typography className="text-[16px] text-[#1C1A1AC9] max-w-[449px] !leading-[25px]">
+                        We help you understand exactly where your money goes by tracking every expense, income, bill, and debt in one clear and organized view.
+                    </Typography>
+                    <Box className="flex flex-col gap-[24px] justify-center items-start">
+                        {featuresList.map((feature, index) => (
+                            <Box key={index} className="flex flex-row gap-[12px] items-center ">
+                                <Box className="w-4 h-4 bg-[#3447AA] rounded-full flex items-center justify-center">
+                                    <Check className="h-3 w-3 text-[#FFFFFF]" />
+                                </Box>
+                                <Typography className="!text-[16px] text-[#1C1A1A] !leading-[25px]">{feature}</Typography>
+                            </Box>
+                        ))}
+                    </Box>
+                    <Box className="w-full">
+                        {progressList.map((progress, index) => (
+                            <Box key={index} className="mb-4">
+                                <Box className="flex flex-row justify-between mb-1">
+                                    <Typography className="!text-[16px] text-[#1C1A1A] mb-1">{progress.label}</Typography>
+                                    <Typography className="!text-[16px] text-[#3447AA]">{progress.value}%</Typography>
+                                </Box>
+                                <Box className="w-full h-[7px] bg-[#E5E7EB] rounded-full">
+                                    <Box
+                                        className="h-[7px] bg-[#FCA3D8] rounded-full"
+                                        sx={{ width: `${progress.value}%` }}
+                                    />
+                                </Box>
+                            </Box>
+                        ))} 
+                    </Box>
+                </Box>
+
+
+                {/*right side*/}
+                <Box className="flex w-[59%] flex-row">
+                    <Box
+                        component="img"
+                        src="/CountingMoney.jpg"
+                        alt="An employee counting money"
+                        className="h-[499px] w-full rounded-s-xl object-cover"
+                    />
+
+                    <Box
+                        className="relative h-[499px] w-full overflow-hidden rounded-e-xl p-[32px]"
+                        sx={{
+                        backgroundImage: "url('/footer-bg.jpg')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                        }}
+                    >
+                        
+                    <Box className="absolute inset-0 bg-[#3447AA]/90" />
+                        <Box className="relative z-10">
+                        <ContactUsForm />
+                        </Box>
+                    </Box>
+                </Box>
+
             </Box>
         </Box>
     );
