6cf7074086572739e364abac318a9a7e947c3d32
noorafifi889 <noorelafifi889@gmail.com>
2026-01-16 18:15:11 +0200

    fixed Hydrtion MUi and solve it

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,29 @@
 "use client"
 
+import * as React from "react"
 import { ThemeProvider, CssBaseline } from "@mui/material"
+import { CacheProvider } from "@emotion/react"
+import createCache from "@emotion/cache"
 import theme from "./theme"
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const cache = React.useMemo(
+    () => createCache({ key: "mui", prepend: true }),
+    []
   )
-}
+
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        {mounted && <CssBaseline />}
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
+  )
+} 
