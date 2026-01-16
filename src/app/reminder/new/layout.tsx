a19829b89ea77f7eacf06e06dc62baa8743d595b
noorafifi889 <noorelafifi889@gmail.com>
2026-01-16 15:32:07 +0200

    clear work to  add reminder page

--- src/app/reminder/new/layout.tsx
+++ src/app/reminder/new/layout.tsx
@@ -5,6 +5,7 @@ import PageHero from "@/src/shared/ui/page-hero/PageHero"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
+
       <PageHero title="Add Reminder" breadcrumb={["Home", "Reminder", "Add"]} />
       {children}
       <Footer />
