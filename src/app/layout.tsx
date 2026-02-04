f063561546572ca2d83a1ccd30637c25204bbe4d
Seraj-Omar <srag20062017@gmail.com>
2026-02-04 18:50:14 +0200

    deleted unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers";
-import Footer from "../components/footer/Footer";
-import Header from "../components/Header";
 import ThemeRegistry from "./theme-registry";
 
 const geistSans = Geist({
