fd5234fe7e61b78fc8f41b025d09269aeb8796dd
PC <asmaasalhaj@gmail.com>
2026-02-05 22:38:30 +0200

    fix: move about-us to main layout

---
+++ src/app/(main)/about-us/layout.tsx
@@ -0,0 +1,35 @@
+
+// import PageHero from "../../shared/ui/page-hero/PageHero";
+
+// export default function AboutLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <>
+//       <PageHero
+//         title="About Us"
+//         breadcrumb={["Home", "Service"]}
+//       />
+//       {children}
+//     </>
+//   )
+// }
+
+import PageHero from "../../../shared/ui/page-hero/PageHero";
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <PageHero
+        title="About Us"
+        breadcrumb={["Home","About Us"]}
+      />
+      {children}
+    </>
+  )
+}
