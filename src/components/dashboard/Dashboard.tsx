2b574062907e36b71d107c7f9c17bf247f564953
Seraj-Omar <srag20062017@gmail.com>
2026-02-07 20:40:00 +0200

    Notifications Section

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -2,11 +2,13 @@
 import {Box} from "@mui/material";
 import Header from "./Header";
 import Stats from "./Stats";
+import Notifications from "./Notifications";
 export default function Dashboard() {
     return (
         <Box className="flex flex-col h-screen bg-[#f9f9fa] gap-4">
             <Header />
             <Stats />
+            <Notifications />
         </Box>
     );
 }
