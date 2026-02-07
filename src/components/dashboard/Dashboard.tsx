7a58ffce4b72878f2cd43a246b9a95ada928468d
Seraj-Omar <srag20062017@gmail.com>
2026-02-07 19:26:04 +0200

    stats section completed

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 "use client";
-import {Box, Typography} from "@mui/material";
+import {Box} from "@mui/material";
 import Header from "./Header";
+import Stats from "./Stats";
 export default function Dashboard() {
     return (
-        <Box className="flex flex-col h-screen bg-[#f9f9fa]">
+        <Box className="flex flex-col h-screen bg-[#f9f9fa] gap-4">
             <Header />
+            <Stats />
         </Box>
     );
 }
