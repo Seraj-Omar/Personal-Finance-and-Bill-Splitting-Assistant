474fda529356dcd257ba3c44e30a47659de1a8fd
Seraj-Omar <srag20062017@gmail.com>
2026-02-12 15:01:34 +0200

    responsive width

--- src/components/dashboard/DashboardTitle.tsx
+++ src/components/dashboard/DashboardTitle.tsx
@@ -3,7 +3,7 @@
 import { Box, Typography } from "@mui/material";
 export default function DashboardTitle({ title, width }: { title: string, width?: number }) {
     return (
-        <Box className="flex flex-col gap-1 rounded-2xl w-full">
+        <Box className="flex flex-col gap-1 rounded-2xl w-full overflow-hidden">
             <Typography component="h2" className="!text-[18px] !font-[500] px-2 pt-2  w-fit">
                 {title}
                 <Box sx={{ filter: "drop-shadow(0 3px 5px rgba(63,81,181,0.5))" }}>
@@ -26,6 +26,18 @@ export default function DashboardTitle({ title, width }: { title: string, width?
                                     },  
                             }}>
                                 <Box sx={{position: "absolute",right: -43,top: -9,width: 18,height: 2,backgroundColor: "#b2b2b2",transform: "translateY(-50%) rotate(147deg)",transformOrigin: "left center",}}/>
+                                <Box
+                                    sx={{flex: 1,height: 2.5,backgroundColor: "#b2b2b2",position: "relative",
+                                        "&::after": {
+                                            content: '""',
+                                            position: "absolute",
+                                            left: "calc(100% + 24px)",
+                                            right: -100000000000,
+                                            top: -10,
+                                            height: 2.5,
+                                            backgroundColor: "#b2b2b2",
+                                        },
+                                }}/>
                             </Box>
 
                         </Box>
@@ -35,19 +47,8 @@ export default function DashboardTitle({ title, width }: { title: string, width?
                 </Box>
 
             </Typography>
+            
 
-            <Box
-                sx={{flex: 1,height: 2.5,backgroundColor: "#b2b2b2",position: "relative",
-                    "&::after": {
-                        content: '""',
-                        position: "absolute",
-                        left: "74%",
-                        right: 0,
-                        top: -18,
-                        height: 2.5,
-                        backgroundColor: "#b2b2b2",
-                    },
-                }}/>
         </Box>
     );
 }
