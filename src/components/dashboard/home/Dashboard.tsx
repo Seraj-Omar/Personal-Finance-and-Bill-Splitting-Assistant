c09a731b60b6e2cd38476ecc033c2f3816e4f0e4
Seraj-Omar <srag20062017@gmail.com>
2026-02-13 10:30:01 +0200

    expenses vs revenues

--- src/components/dashboard/home/Dashboard.tsx
+++ src/components/dashboard/home/Dashboard.tsx
@@ -6,6 +6,8 @@ import Notifications from "./Notifications";
 import FinancialSnapshot from "./FinancialSnapshot";
 import FeatureUsageSummary from "./FeatureUsageSummary";
 import PeakUsageHours from "./PeakUsageHours";
+import ExpensesVsRevenues from "./ExpensesVsRevenues";
+import MonthlyOverview from "./MonthlyOverview";
 export default function Dashboard() {
     return (
         <Box className="flex flex-col h-screen bg-[#f9f9fa] gap-4 pt-4">
@@ -16,6 +18,13 @@ export default function Dashboard() {
                 <FinancialSnapshot />
                 <FeatureUsageSummary />
                 <PeakUsageHours />
+            </Box>
+            <Box className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                <Box className="col-span-1 lg:col-span-2">
+                    <ExpensesVsRevenues />
+                </Box>
+                <Box className="col-span-1">
+                </Box>
             </Box>
         </Box>
     );
