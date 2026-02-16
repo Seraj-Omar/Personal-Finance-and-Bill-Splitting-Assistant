e93d46533b8eed92fb3bc1e02a9ea1e53c30f405
Noor Al-Afifi <124470858+noorafifi889@users.noreply.github.com>
2026-02-16 18:09:13 +0200

    Revert "Feature/home dashboard"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import Dashboard from "../../components/dashboard/home/Dashboard";
+import { redirect } from "next/navigation";
+
 export default function DashboardPage() {
-    return (<Dashboard />);
+  redirect("/dashboard/settings");
 }
