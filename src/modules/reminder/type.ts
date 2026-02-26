c47473eca1e30b9c34bb9545d70196652cd15018
noorafifi889 <noorelafifi889@gmail.com>
2026-02-26 23:50:33 +0200

    add a type to reminder component

--- src/modules/reminder/type.ts
+++ src/modules/reminder/type.ts
@@ -1,8 +1,38 @@
-export type Bill = {
-  id: string
-  title: string
-  value: number
-  frequency: "Monthly" | "Weekly" | "Yearly"
-  date: string
-  active: boolean
-}
+export type ReminderFrequency = "NONE" | "DAILY" | "WEEKLY" | "MONTHLY";
+
+export type Reminder = {
+  id: string;
+  userId: string;
+  dueDate: string; // "2026-03-01"
+  description: string;
+  isActive: boolean;
+  frequency: ReminderFrequency;
+  nextRemindAt: string; // ISO
+  lastSentAt: string | null;
+  completedAt: string | null;
+  debtId: any;
+  billId: any;
+  expenseId: any;
+  groupInvoiceId: any;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type PaginationMeta = {
+  page: number;
+  limit: number;
+  totalPages: number;
+  total: number;
+};
+
+export type ApiResponse<T> = {
+  success: boolean;
+  meta: PaginationMeta;
+  message: string;
+  data: T;
+};
+
+export type GetMyRemindersParams = {
+  page?: number;
+  limit?: number;
+};
