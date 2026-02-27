023db792b8c1797c9c1f4a89989e1dffd392753e
Nour-Anwar <nooranwar067@gmail.com>
2026-02-27 17:27:50 +0200

    Insights with api

--- src/components/reportComponents/Insights.tsx
+++ src/components/reportComponents/Insights.tsx
@@ -3,117 +3,144 @@
 import { useState } from "react";
 import Image from "next/image";
 
-export default function Insights() {
-  const [selectedAlert, setSelectedAlert] = useState<number | null>(null);
+interface Insight {
+  id: string;
+  type: "DANGER" | "WARNING" | "SUCCESS";
+  title: string;
+  message: string;
+  isRead: boolean;
+}
 
-  const alerts = [
-    {
-      type: "danger",
-      bg: "bg-[#FDEAE7]",
-      icon: (
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g clipPath="url(#clip0_1744_1589)">
-            <path
-              d="M3.375 9.00001L10.656 2.62051C11.631 1.76626 13.125 2.48401 13.125 3.80626V9.00001H3.375Z"
-              fill="#44BC59"
-            />
-            <path
-              d="M6.375 9.00013L14.2714 5.06263C15.2715 4.51663 16.5 5.22763 16.5 6.35188V9.00013H6.375Z"
-              fill="#2D9739"
-            />
-            <path
-              d="M21.375 18.375V21C21.375 21.5967 21.1379 22.169 20.716 22.591C20.294 23.0129 19.7217 23.25 19.125 23.25H3C2.40326 23.25 1.83097 23.0129 1.40901 22.591C0.987053 22.169 0.75 21.5967 0.75 21V11.25C0.75 10.6533 0.987053 10.081 1.40901 9.65901C1.83097 9.23705 2.40326 9 3 9H19.125C19.7217 9 20.294 9.23705 20.716 9.65901C21.1379 10.081 21.375 10.6533 21.375 11.25V13.875V18.375Z"
-              fill="#624434"
-            />
-            <path
-              d="M17.8376 23.25H19.125C19.7217 23.25 20.294 23.0129 20.716 22.591C21.1379 22.169 21.375 21.5967 21.375 21V11.25C21.375 10.6533 21.1379 10.081 20.716 9.65901C20.294 9.23705 19.7217 9 19.125 9H5.625C5.625 15.9472 10.8431 21.7586 17.8376 23.25Z"
-              fill="#755640"
-            />
-            <path
-              d="M21.75 18.375H17.25C16.0073 18.375 15.75 17.3677 15.75 16.125C15.75 14.8823 16.0073 13.875 17.25 13.875H21.75C22.1478 13.875 22.5294 14.033 22.8107 14.3143C23.092 14.5956 23.25 14.9772 23.25 15.375V16.875C23.25 17.2728 23.092 17.6544 22.8107 17.9357C22.5294 18.217 22.1478 18.375 21.75 18.375Z"
-              fill="#624434"
-            />
-            <path
-              d="M18.375 17.25C18.9963 17.25 19.5 16.7463 19.5 16.125C19.5 15.5037 18.9963 15 18.375 15C17.7537 15 17.25 15.5037 17.25 16.125C17.25 16.7463 17.7537 17.25 18.375 17.25Z"
-              fill="#F2F2F2"
-            />
-            <path
-              d="M19.8679 3.75073C19.9795 4.29901 19.9184 4.86842 19.6929 5.38051C19.4675 5.89261 19.0888 6.3222 18.609 6.6101L18.375 6.75073L19.875 7.87573L19.941 7.83185C20.6128 7.38387 21.1636 6.77699 21.5446 6.06507C21.9256 5.35314 22.1249 4.55819 22.125 3.75073H22.9804C23.0326 3.75076 23.0837 3.73529 23.1271 3.70628C23.1706 3.67728 23.2044 3.63604 23.2244 3.58778C23.2444 3.53952 23.2496 3.48642 23.2394 3.43518C23.2292 3.38395 23.2041 3.3369 23.1671 3.29998L21.4421 0.979479C21.3901 0.909458 21.3224 0.852589 21.2444 0.81342C21.1664 0.774251 21.0804 0.75387 20.9932 0.753906C20.9059 0.753943 20.8199 0.774396 20.742 0.81363C20.664 0.852864 20.5964 0.90979 20.5444 0.979854L18.8299 3.28873C18.7919 3.32652 18.766 3.37474 18.7555 3.42727C18.745 3.4798 18.7503 3.53427 18.7708 3.58377C18.7913 3.63327 18.826 3.67556 18.8706 3.70529C18.9152 3.73501 18.9676 3.75083 19.0211 3.75073H19.8679Z"
-              fill="#FFC239"
-            />
-            <path
-              d="M21.5557 1.13281L19.9549 3.28869C19.9169 3.32648 19.891 3.3747 19.8805 3.42723C19.87 3.47976 19.8753 3.53423 19.8958 3.58373C19.9163 3.63323 19.951 3.67552 19.9956 3.70525C20.0402 3.73497 20.0926 3.75079 20.1461 3.75069H20.9929C21.1046 4.29897 21.0436 4.86842 20.8181 5.38054C20.5926 5.89266 20.2139 6.32224 19.734 6.61006L19.5 6.75069L20.4364 7.45306C20.9664 6.99272 21.3914 6.42397 21.6827 5.78525C21.9741 5.14653 22.1249 4.45271 22.125 3.75069H22.9804C23.0326 3.75072 23.0837 3.73525 23.1271 3.70624C23.1706 3.67724 23.2044 3.63599 23.2244 3.58774C23.2444 3.53948 23.2496 3.48637 23.2394 3.43514C23.2292 3.38391 23.2041 3.33686 23.1671 3.29994L21.5557 1.13281Z"
-              fill="#FFD55D"
-            />
-          </g>
-          <defs>
-            <clipPath id="clip0_1744_1589">
-              <rect width="24" height="24" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
-      ),
-      title: "Spending Alert",
-      text: "You spent 32% more on Food compared to last month",
-      comparison: `Current: $178.40 
-       Last month: $135.15  
-       +$43.25 (+32%)`,
-    },
-    {
-      type: "warning",
-      bg: "bg-[#FCF4EA]",
-      icon: (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path
-            d="M12 2L2 22H22L12 2Z"
-            fill="#F59E0B"
-            stroke="#D97706"
-            strokeWidth="1.5"
-          />
-          <path
-            d="M12 8v4m0 4h.01"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-          />
-        </svg>
-      ),
-      title: "Spending Alert",
-      text: "You spent more on Food compared to last month",
-      comparison: `Current: $156.80
-Last month: $142.30
-+$14.50 (+10%)`,
-    },
-    {
-      type: "success",
-      bg: "bg-[#EAF7EF]",
-      icon: (
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M22.1456 2.63432C17.9528 4.01072 12.6073 7.6916 7.67842 13.9969L4.7653 10.7641C4.31722 10.2522 3.48514 10.2522 3.03682 10.7641L0.892663 13.165C0.476503 13.645 0.508663 14.3492 0.956743 14.7654L7.55002 21.1026C8.0941 21.6147 8.99026 21.4866 9.3745 20.8146C12.8953 14.445 16.6722 9.70808 23.0737 4.26656C23.8417 3.59456 23.1375 2.31416 22.1454 2.63456L22.1456 2.63432Z"
-            fill="#7CA489"
-          />
-        </svg>
-      ),
-      title: "Spending Alert",
-      text: "You spent less on Food compared to last month",
-      comparison: `Current: $98.20 
-       Last month: $135.00 
-       -$36.80 (-27%)`,
-    },
-  ];
+const getBgColor = (type: Insight["type"]) => {
+  switch (type) {
+    case "DANGER":
+      return "bg-[#FDEAE7]";
+    case "WARNING":
+      return "bg-[#FCF4EA]";
+    case "SUCCESS":
+      return "bg-[#EAF7EF]";
+    default:
+      return "bg-gray-50";
+  }
+};
+
+const typeIcons: Record<Insight["type"], JSX.Element> = {
+  DANGER: (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_1744_1589)">
+        <path
+          d="M3.375 9.00001L10.656 2.62051C11.631 1.76626 13.125 2.48401 13.125 3.80626V9.00001H3.375Z"
+          fill="#44BC59"
+        />
+        <path
+          d="M6.375 9.00013L14.2714 5.06263C15.2715 4.51663 16.5 5.22763 16.5 6.35188V9.00013H6.375Z"
+          fill="#2D9739"
+        />
+        <path
+          d="M21.375 18.375V21C21.375 21.5967 21.1379 22.169 20.716 22.591C20.294 23.0129 19.7217 23.25 19.125 23.25H3C2.40326 23.25 1.83097 23.0129 1.40901 22.591C0.987053 22.169 0.75 21.5967 0.75 21V11.25C0.75 10.6533 0.987053 10.081 1.40901 9.65901C1.83097 9.23705 2.40326 9 3 9H19.125C19.7217 9 20.294 9.23705 20.716 9.65901C21.1379 10.081 21.375 10.6533 21.375 11.25V13.875V18.375Z"
+          fill="#624434"
+        />
+        <path
+          d="M17.8376 23.25H19.125C19.7217 23.25 20.294 23.0129 20.716 22.591C21.1379 22.169 21.375 21.5967 21.375 21V11.25C21.375 10.6533 21.1379 10.081 20.716 9.65901C20.294 9.23705 19.7217 9 19.125 9H5.625C5.625 15.9472 10.8431 21.7586 17.8376 23.25Z"
+          fill="#755640"
+        />
+        <path
+          d="M21.75 18.375H17.25C16.0073 18.375 15.75 17.3677 15.75 16.125C15.75 14.8823 16.0073 13.875 17.25 13.875H21.75C22.1478 13.875 22.5294 14.033 22.8107 14.3143C23.092 14.5956 23.25 14.9772 23.25 15.375V16.875C23.25 17.2728 23.092 17.6544 22.8107 17.9357C22.5294 18.217 22.1478 18.375 21.75 18.375Z"
+          fill="#624434"
+        />
+        <path
+          d="M18.375 17.25C18.9963 17.25 19.5 16.7463 19.5 16.125C19.5 15.5037 18.9963 15 18.375 15C17.7537 15 17.25 15.5037 17.25 16.125C17.25 16.7463 17.7537 17.25 18.375 17.25Z"
+          fill="#F2F2F2"
+        />
+        <path
+          d="M19.8679 3.75073C19.9795 4.29901 19.9184 4.86842 19.6929 5.38051C19.4675 5.89261 19.0888 6.3222 18.609 6.6101L18.375 6.75073L19.875 7.87573L19.941 7.83185C20.6128 7.38387 21.1636 6.77699 21.5446 6.06507C21.9256 5.35314 22.1249 4.55819 22.125 3.75073H22.9804C23.0326 3.75076 23.0837 3.73529 23.1271 3.70628C23.1706 3.67728 23.2044 3.63604 23.2244 3.58778C23.2444 3.53952 23.2496 3.48642 23.2394 3.43518C23.2292 3.38395 23.2041 3.3369 23.1671 3.29998L21.4421 0.979479C21.3901 0.909458 21.3224 0.852589 21.2444 0.81342C21.1664 0.774251 21.0804 0.75387 20.9932 0.753906C20.9059 0.753943 20.8199 0.774396 20.742 0.81363C20.664 0.852864 20.5964 0.90979 20.5444 0.979854L18.8299 3.28873C18.7919 3.32652 18.766 3.37474 18.7555 3.42727C18.745 3.4798 18.7503 3.53427 18.7708 3.58377C18.7913 3.63327 18.826 3.67556 18.8706 3.70529C18.9152 3.73501 18.9676 3.75083 19.0211 3.75073H19.8679Z"
+          fill="#FFC239"
+        />
+        <path
+          d="M21.5557 1.13281L19.9549 3.28869C19.9169 3.32648 19.891 3.3747 19.8805 3.42723C19.87 3.47976 19.8753 3.53423 19.8958 3.58373C19.9163 3.63323 19.951 3.67552 19.9956 3.70525C20.0402 3.73497 20.0926 3.75079 20.1461 3.75069H20.9929C21.1046 4.29897 21.0436 4.86842 20.8181 5.38054C20.5926 5.89266 20.2139 6.32224 19.734 6.61006L19.5 6.75069L20.4364 7.45306C20.9664 6.99272 21.3914 6.42397 21.6827 5.78525C21.9741 5.14653 22.1249 4.45271 22.125 3.75069H22.9804C23.0326 3.75072 23.0837 3.73525 23.1271 3.70624C23.1706 3.67724 23.2044 3.63599 23.2244 3.58774C23.2444 3.53948 23.2496 3.48637 23.2394 3.43514C23.2292 3.38391 23.2041 3.33686 23.1671 3.29994L21.5557 1.13281Z"
+          fill="#FFD55D"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1744_1589">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
+  WARNING: (
+    <svg
+      width="23"
+      height="21"
+      viewBox="0 0 23 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M22.3834 16.8721L15.6346 4.86494C15.0617 3.84494 14.4898 2.82974 13.9186 1.80998C13.609 1.20758 13.2231 0.578302 12.5794 0.288382C12.1445 0.0855819 11.7183 -0.026018 11.2354 0.00518195C10.4129 0.052462 9.6442 0.514222 9.205 1.20998C9.05788 1.47038 8.9158 1.73246 8.76364 1.99238C8.15188 3.08558 7.53364 4.17998 6.92044 5.27318C4.7218 9.26102 2.39572 13.1829 0.260683 17.2033C0.287083 17.0977 0.335083 16.9993 0.385483 16.9057C-0.664517 18.5802 0.553963 20.8619 2.55028 20.8487C2.84068 20.8511 3.13348 20.8487 3.42388 20.8487H20.238C22.2579 20.8307 23.4509 18.5766 22.3836 16.8719L22.3834 16.8721ZM12.3082 17.1025C11.5107 17.9058 10.0988 17.3219 10.105 16.1905C10.0848 15.4981 10.7098 14.8881 11.3962 14.8993C12.53 14.8888 13.117 16.3146 12.3082 17.1025ZM12.685 12.2329C12.6929 13.3672 11.2719 13.9513 10.4818 13.1449C10.2514 12.9097 10.105 12.5617 10.105 12.2329V8.63534C10.1698 6.92966 12.6236 6.93134 12.685 8.63534V12.2329Z"
+        fill="#FFC100"
+      />
+      <path
+        d="M0.259766 17.2038C0.286166 17.0982 0.334166 16.9998 0.384566 16.9062C0.346166 17.0047 0.302966 17.103 0.259766 17.2038Z"
+        fill="#FFC100"
+      />
+    </svg>
+  ),
+  SUCCESS: (
+    <svg
+      width="23"
+      height="19"
+      viewBox="0 0 23 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21.546 0.0503374C17.3532 1.42674 12.0077 5.10762 7.07881 11.4129L4.16569 8.1801C3.71761 7.66818 2.88553 7.66818 2.43721 8.1801L0.293054 10.5811C-0.123106 11.0611 -0.090946 11.7652 0.357134 12.1814L6.95041 18.5186C7.49449 19.0307 8.39065 18.9026 8.77489 18.2306C12.2957 11.861 16.0726 7.1241 22.4741 1.68258C23.2421 1.01058 22.5379 -0.269823 21.5458 0.0505774L21.546 0.0503374Z"
+        fill="#7CA489"
+      />
+    </svg>
+  ),
+};
+
+const mockInsights: Insight[] = [
+  {
+    id: "1",
+    type: "DANGER",
+    title: "High Spending",
+    message: "Entertainment expenses are 35% higher than usual",
+    isRead: false,
+  },
+  {
+    id: "2",
+    type: "WARNING",
+    title: "Budget Alert",
+    message: "You have exceeded 80% of your Food budget",
+    isRead: false,
+  },
+  {
+    id: "3",
+    type: "SUCCESS",
+    title: "Savings Boost",
+    message: "Your savings increased by 18% compared to last month",
+    isRead: false,
+  },
+];
+
+export default function Insights() {
+  const [insights, setInsights] = useState<Insight[]>(mockInsights);
+  const [selectedInsight, setSelectedInsight] = useState<Insight | null>(null);
+
+  const handleCloseModal = () => {
+    if (!selectedInsight) return;
+    setInsights((prev) => prev.filter((i) => i.id !== selectedInsight.id));
+    setSelectedInsight(null);
+  };
 
   return (
     <div className="flex items-center justify-center w-[89%] mb-[63px] gap-[5%]">
@@ -128,44 +155,48 @@ Last month: $142.30
           activity.
         </p>
 
-        <div className="flex flex-col gap-[24px] ">
-          {alerts.map((alert, index) => (
-            <div
-              key={index}
-              onClick={() => setSelectedAlert(index)}
-              className={`flex items-center justify-between w-full  h-[97px] ${alert.bg} rounded-[16px] p-4 cursor-pointer hover:opacity-90 transition-opacity border border-transparent hover:border-gray-200`}
-            >
-              <div className="flex gap-3">
-                {alert.icon}
-                <div>
-                  <span className="text-[16px] font-medium block">
-                    {alert.title}
-                  </span>
-                  <span className="text-[14px] text-[#707070]">
-                    {alert.text}
-                  </span>
-                </div>
-              </div>
-
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+        {insights.length === 0 ? (
+          <p className="text-gray-400">No insights available.</p>
+        ) : (
+          <div className="flex flex-col gap-[24px]">
+            {insights.map((insight) => (
+              <div
+                key={insight.id}
+                onClick={() => setSelectedInsight(insight)}
+                className={`flex items-center justify-between w-full h-[97px] 
+                  ${getBgColor(insight.type)} rounded-[16px] p-4 cursor-pointer hover:opacity-90 transition-opacity border border-transparent hover:border-gray-200`}
               >
-                <path
-                  d="M9.00016 4.07992L15.5202 10.5999C16.2902 11.3699 16.2902 12.6299 15.5202 13.3999L9.00016 19.9199"
-                  stroke="#1C1A1A"
-                  strokeWidth="1.5"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          ))}
-        </div>
+                <div className="flex gap-3 items-center">
+                  {typeIcons[insight.type]}
+                  <div>
+                    <span className="text-[16px] font-medium block">
+                      {insight.title}
+                    </span>
+                    <span className="text-[14px] text-[#707070]">
+                      {insight.message}
+                    </span>
+                  </div>
+                </div>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.00016 4.07992L15.5202 10.5999C16.2902 11.3699 16.2902 12.6299 15.5202 13.3999L9.00016 19.9199"
+                    stroke="#1C1A1A"
+                    strokeWidth="1.5"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
       <div className="relative w-full lg:w-[50%] h-[444px] rounded-[16px] hidden lg:block overflow-hidden">
@@ -178,38 +209,34 @@ Last month: $142.30
         />
       </div>
 
-      {selectedAlert !== null && (
+      {/* Modal */}
+      {selectedInsight && (
         <div
           className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4"
-          onClick={() => setSelectedAlert(null)}
+          onClick={handleCloseModal}
         >
           <div
             className="bg-white rounded-2xl p-6 md:p-8 max-w-md w-full"
             onClick={(e) => e.stopPropagation()}
           >
             <div className="flex justify-between items-center mb-6">
-              <h3 className="text-xl font-semibold">
-                {alerts[selectedAlert].title} Details
-              </h3>
+              <h3 className="text-xl font-semibold">{selectedInsight.title}</h3>
               <button
-                onClick={() => setSelectedAlert(null)}
+                onClick={handleCloseModal}
                 className="text-2xl text-gray-500 hover:text-gray-800"
               >
                 ×
               </button>
             </div>
 
-            <p className="text-gray-700 mb-4">{alerts[selectedAlert].text}</p>
-
-            <div className="bg-gray-50 p-5 rounded-xl">
-              <p className="font-medium whitespace-pre-line">
-                {alerts[selectedAlert].comparison}
-              </p>
+            <div className="flex gap-3 mb-4 items-center">
+              {typeIcons[selectedInsight.type]}
+              <span className="text-[#707070]">{selectedInsight.message}</span>
             </div>
 
             <div className="mt-6 flex justify-end">
               <button
-                onClick={() => setSelectedAlert(null)}
+                onClick={handleCloseModal}
                 className="px-6 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
               >
                 Close
