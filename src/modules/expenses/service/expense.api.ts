ab0baf8596708866fbe3f3a7c0408eba1a0e0ca2
noorafifi889 <noorelafifi889@gmail.com>
2026-03-05 15:55:04 +0200

    add a parme to api

--- src/modules/expenses/service/expense.api.ts
+++ src/modules/expenses/service/expense.api.ts
@@ -1,4 +1,5 @@
 import { apiFetch } from "@/src/lib/api";
+import {  Period } from "@/src/types/report/expenses";
 
 export type ExpensesOverviewData = {
   totalBalance: string;
@@ -43,9 +44,28 @@ export type DonutItem = {
   percentage: number;
 };
 
-export function fetchExpensesDonutChart() {
+
+type FetchParams = {
+  currencyId?: string;
+  period?: Period;
+  month?: number;
+  from?: string;
+  to?: string;
+};
+
+export function fetchExpensesDonutChart(params?: FetchParams) {
+  const query = new URLSearchParams();
+
+  if (params?.currencyId) query.append("currencyId", params.currencyId);
+  if (params?.period) query.append("period", params.period);
+  if (params?.month) query.append("month", String(params.month));
+  if (params?.from) query.append("from", params.from);
+  if (params?.to) query.append("to", params.to);
+
+  const qs = query.toString();
+
   return apiFetch<ApiResponse<DonutItem[]>>(
-    "/expenses/charts/donut",
+    `/expenses/charts/donut${qs ? `?${qs}` : ""}`,
     { method: "GET" }
   );
 }
